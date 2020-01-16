f0f0bab40ed8ca46cc6c16ee0c01c89219e80ca1
Mason <liujianfeng@kingdee.com>
2020-01-16 17:51:24 +0800

    leetcode 删除排序数组中的重复项

--- leetcode算法/1.两数之和.js
+++ leetcode算法/1.两数之和.js
@@ -32,12 +32,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
     }
-  }
 };
+
+
 
